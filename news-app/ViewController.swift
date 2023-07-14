089f9777fff1cd6a2098d70ea593c3801c573e7a
Hüseyin Temel <huseyintemeel@gmail.com>
2023-07-14 09:23:53 +0300

    Initial Commit

---
+++ news-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news-app
+//
+//  Created by huseyin on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
